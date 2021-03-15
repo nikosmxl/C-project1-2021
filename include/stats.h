d98041c25032835d4249f70016e32cdee4943bb9
nikosmxl <sdi2000133@uoa.gr>
2021-03-16 00:30:29 +0200

    changed generic max

--- include/stats.h
+++ include/stats.h
@@ -12,3 +12,16 @@ int stats_find_min(int array[], int size);
 // Επιστρέφει το μεγαλύτερο στοιχείο του array με μέγεθος size (ΙΝΤ_MIN αν size == 0)
 
 int stats_find_max(int array[], int size);
+
+
+// Πιο κατανοητό να γράφουμε "Pointer" παρά "void*"
+typedef void* Pointer;
+
+// Μια μεταβλητή τύπου CompareFunc είναι δείκτης σε συνάρτηση που
+// συγκρίνει 2 στοιχεία a και b και επιστρέφει έναν ακέραιο με τιμή:
+// < 0  αν a <  b
+//   0  αν a == b
+// > 0  αν a >  b
+typedef int (*CompareFunc)(Pointer a, Pointer b);
+
+Pointer generic_max(Pointer a, Pointer b, CompareFunc comp);
--- programs/generic_max/generic_max.c
+++ programs/generic_max/generic_max.c
@@ -1,28 +1,8 @@
 // Υλοποίηση μιας γενικής συνάρτησης max
 
 #include <stdio.h>
-
-// Πιο κατανοητό να γράφουμε "Pointer" παρά "void*"
-typedef void* Pointer;
-
-// Μια μεταβλητή τύπου CompareFunc είναι δείκτης σε συνάρτηση που
-// συγκρίνει 2 στοιχεία a και b και επιστρέφει έναν ακέραιο με τιμή:
-// < 0  αν a <  b
-//   0  αν a == b
-// > 0  αν a >  b
-typedef int (*CompareFunc)(Pointer a, Pointer b);
-
-
-// Επιστρέφει το μεγαλύτερο από τα a, b, όπου η σύγκριση γίνεται
-// με βάση τη συνάρτηση compare.
-
-Pointer generic_max(Pointer a, Pointer b, CompareFunc comp) {
-    // Καλούμε την comp για να μας συγκρίνει τους αριθμούς, επιστρέφουμε a ή b ανάλογα
-    if(comp(a, b) > 0)
-        return a;
-    else
-        return b;
-}
+#include <string.h>
+#include "stats.h"
 
 // Συγκρίνει 2 ακεραίους a, b
 
@@ -30,6 +10,14 @@ int compare_ints(Pointer a, Pointer b) {
     int* ia = a;
     int* ib = b;
     return *ia - *ib;       // αρνητικός, μηδέν ή θετικός, ανάλογα με τη διάταξη των a,b
+}
+
+int compare_chars(Pointer a, Pointer b){
+    char* ca = a;
+    char* cb = b;
+    
+    return strcmp(ca, cb);
+    
 }
 
 int main() {
@@ -42,9 +30,9 @@ int main() {
 
 	// Κάντε uncomment τα παρακάτω και συμπληρώστε κατάλληλα
 
-    // char* s1 = "zzz";
-    // char* s2 = "aaa";
-	// char* max_s = ...;
+    char* s1 = "zzz";
+    char* s2 = "aaa";
+	char* max_s = generic_max(s1, s2, compare_chars);
 
-    // printf("max of %s , %s is %s\n", s1, s2, max_s);
+    printf("max of %s , %s is %s\n", s1, s2, max_s);
 }
